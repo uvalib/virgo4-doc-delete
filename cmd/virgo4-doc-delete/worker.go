47324a72f4cb49455d16ee3546f1728725b43d6b
UVADave <dpg3k@virginia.edu>
2019-11-08 09:49:27 -0500

    Latest SDK

--- cmd/virgo4-doc-delete/worker.go
+++ cmd/virgo4-doc-delete/worker.go
@@ -41,7 +41,7 @@ func worker(id int, config ServiceConfig, aws awssqs.AWS_SQS, queue1 awssqs.Queu
 				// send the block
 				err := sendOutboundMessages(config, aws, queue1, queue2, messages)
 				if err != nil {
-					if err != awssqs.OneOrMoreOperationsUnsuccessfulError {
+					if err != awssqs.ErrOneOrMoreOperationsUnsuccessful {
 						fatalIfError(err)
 					}
 				}
@@ -62,7 +62,7 @@ func worker(id int, config ServiceConfig, aws awssqs.AWS_SQS, queue1 awssqs.Queu
 				// send the block
 				err := sendOutboundMessages(config, aws, queue1, queue2, messages)
 				if err != nil {
-					if err != awssqs.OneOrMoreOperationsUnsuccessfulError {
+					if err != awssqs.ErrOneOrMoreOperationsUnsuccessful {
 						fatalIfError(err)
 					}
 				}
@@ -96,13 +96,13 @@ func sendOutboundMessages(config ServiceConfig, aws awssqs.AWS_SQS, queue1 awssq
 
 	opStatus, err1 := aws.BatchMessagePut(queue1, batch)
 	if err1 != nil {
-		if err1 != awssqs.OneOrMoreOperationsUnsuccessfulError {
+		if err1 != awssqs.ErrOneOrMoreOperationsUnsuccessful {
 			return err1
 		}
 	}
 
 	// if one or more message failed to send, report the error
-	if err1 == awssqs.OneOrMoreOperationsUnsuccessfulError {
+	if err1 == awssqs.ErrOneOrMoreOperationsUnsuccessful {
 
 		// check the operation results
 		for ix, op := range opStatus {
@@ -114,13 +114,13 @@ func sendOutboundMessages(config ServiceConfig, aws awssqs.AWS_SQS, queue1 awssq
 
 	opStatus, err2 := aws.BatchMessagePut(queue2, batch)
 	if err2 != nil {
-		if err2 != awssqs.OneOrMoreOperationsUnsuccessfulError {
+		if err2 != awssqs.ErrOneOrMoreOperationsUnsuccessful {
 			return err2
 		}
 	}
 
 	// if one or more message failed to send, report the error
-	if err2 == awssqs.OneOrMoreOperationsUnsuccessfulError {
+	if err2 == awssqs.ErrOneOrMoreOperationsUnsuccessful {
 
 		// check the operation results
 		for ix, op := range opStatus {
@@ -131,8 +131,8 @@ func sendOutboundMessages(config ServiceConfig, aws awssqs.AWS_SQS, queue1 awssq
 	}
 
 	// report that some of the messages were not processed
-	if err1 == awssqs.OneOrMoreOperationsUnsuccessfulError || err2 == awssqs.OneOrMoreOperationsUnsuccessfulError {
-		return awssqs.OneOrMoreOperationsUnsuccessfulError
+	if err1 == awssqs.ErrOneOrMoreOperationsUnsuccessful || err2 == awssqs.ErrOneOrMoreOperationsUnsuccessful {
+		return awssqs.ErrOneOrMoreOperationsUnsuccessful
 	}
 
 	return nil
