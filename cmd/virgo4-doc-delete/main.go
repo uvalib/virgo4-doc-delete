9f79b6b17091c3e341070223c4964fb9b4a48961
Dave Goldstein <dpg3k@virginia.edu>
2022-05-31 12:14:03 -0400

    Move to the uva-aws-s3 helper module

--- cmd/virgo4-doc-delete/main.go
+++ cmd/virgo4-doc-delete/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
 
+	"github.com/uvalib/uva-aws-s3-sdk/uva-s3"
 	"github.com/uvalib/virgo4-sqs-sdk/awssqs"
 )
 
@@ -27,6 +29,10 @@ func main() {
 
 	// load our AWS_SQS helper object
 	aws, err := awssqs.NewAwsSqs(awssqs.AwsSqsConfig{MessageBucketName: cfg.MessageBucketName})
+	fatalIfError(err)
+
+	// load our AWS s3 helper object
+	s3Svc, err := uva_s3.NewUvaS3(uva_s3.UvaS3Config{Logging: true})
 	fatalIfError(err)
 
 	// get the queue handles from the queue name
@@ -73,8 +79,15 @@ func main() {
 				continue
 			}
 
+			// create temp file
+			tmp, e := ioutil.TempFile(cfg.DownloadDir, "")
+			fatalIfError(e)
+			tmp.Close()
+			file.LocalName = tmp.Name()
+
 			// download the file
-			file.LocalName, e = s3download(cfg.DownloadDir, f.SourceBucket, f.SourceKey, f.ObjectSize)
+			o := uva_s3.NewUvaS3Object(f.SourceBucket, f.SourceKey)
+			e = s3Svc.GetToFile(o, file.LocalName)
 			fatalIfError(e)
 
 			// update our lost of files to be processed
